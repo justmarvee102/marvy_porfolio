7b11320b52c2d7031f1a8076bc2bf88704157b5a
Emmanuel Marvelous <emmanuelmarvelous579@gmail.com>
2024-02-23 00:15:17 +0100

    Setting up my portfolio

---
+++ src/container/Testimonial/Testimonial.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import '<div className="" />
+<Testimonial></Testimonial>.scss';
+
+const Testimonial = () => {
+  return (
+    <div>Testimonial</div>
+  )
+}
+
+export default Testimonial
